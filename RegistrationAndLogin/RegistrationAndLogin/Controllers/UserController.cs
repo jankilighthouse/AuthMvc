5b3793f5d813b485e0d752d76356e45c14408cc6
Janki Patel <jankipatel6890@gmail.com>
2018-12-24 19:05:45 -0700

    Implemented Validation of registration page

---
+++ RegistrationAndLogin/RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using RegistrationAndLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //Registration POST Action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude= "IsEmailVerified,ActivationCode")] User user) //this two properties are not in user table so this bind operation automatically add this data in result
+        {
+            bool Status = false;
+            string message = "";
+            //
+            //Model validation
+            if(ModelState.IsValid)
+            {
+                #region //Email alredy exist or not
+                var isExist = IsEmailExist(user.EmailID);
+                if(isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exist");
+                    return View(user);
+                }
+                #endregion
+
+                #region //Generate Activation Code
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                #endregion
+                user.IsEmailVerified = false;
+
+                #region Save to Database
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+                }
+                //Send Email to user
+                SendVerificationLinkEmail(user.EmailID, user.ActivationCode.ToString());
+
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+
+            //Passwod Hashing
+
+
+            return View(user);
+        }
+
+        //Verify Email
+
+        //Verify Email Link
+
+        //Login
+
+        //Login POST
+
+        //Logout
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID,string activationCode)
+        {
+           
+        }
+    }
+    
+}
---
+++ RegistrationAndLogin/RegistrationAndLogin/Models/Extended/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationAndLogin.Models
+{
+    //Extended partial class from users.cs for validation
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; } //this attribute is not in our user database ,so this value will not stored in user table
+
+    }
+    public class UserMetaData
+    {
+        [Display(Name ="First Name")]
+        [Required(AllowEmptyStrings = false,ErrorMessage ="First name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name required")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Email ID")]
+        [DataType(DataType.DateTime)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID required")]
+        public string EmailID { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true,DataFormatString ="{0:MM/dd/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Passwords is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage ="Minimum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Confirm password amd  password do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
